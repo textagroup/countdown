0e96d03daa19fff37aaccd2bd216a7f34e7f0b62
James Ayers <james.ayers@dna.co.nz>
2015-08-12 11:33:54 +1200

    Wrapped countdown parts in spans for easier styling

--- js/countdown-coupon-days.js
+++ js/countdown-coupon-days.js
@@ -1,6 +1,7 @@
 jQuery(function ($) {
 	var endDate = new Date('$EndDate');
+	var template = '<span class="days">%D</span> <span class="text-days">days</span> <span class="time">%H:%M:%S</span>';
 	$('#$ElementID').countdown(endDate, function(event) {
-		$(this).html(event.strftime('%D days %H:%M:%S'));
+		$(this).html(event.strftime(template));
 	});
 });
--- js/countdown-days-only.js
+++ js/countdown-days-only.js
@@ -1,6 +1,7 @@
 jQuery(function ($) {
 	var endDate = new Date('$EndDate');
+	var template = '<span class="days">%D</span> <span class="text-days">days</span>';
 	$('#$ElementID').countdown(endDate, function(event) {
-		$(this).html(event.strftime('%D days'));
+		$(this).html(event.strftime(template));
 	});
 });
